937840967481e3b4739b76c2c86be0dff95dcc32
rodolfolopezp <rodolfolopezp@hotmail.com>
2022-03-28 17:58:19 -0600

    Button

--- TelerikWinFormsApp1/TelerikWinFormsApp1/UI/OutlookCalendarForm.Designer.cs
+++ TelerikWinFormsApp1/TelerikWinFormsApp1/UI/OutlookCalendarForm.Designer.cs
@@ -31,6 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OutlookCalendarForm));
             this.calendarUserControl1 = new TelerikWinFormsApp1.UI.CalendarUserControl();
             this.telerikMetroBlueTheme1 = new Telerik.WinControls.Themes.TelerikMetroBlueTheme();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
             this.SuspendLayout();
             // 
@@ -39,14 +40,24 @@
             this.calendarUserControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.calendarUserControl1.Location = new System.Drawing.Point(0, 0);
             this.calendarUserControl1.Name = "calendarUserControl1";
-            this.calendarUserControl1.Size = new System.Drawing.Size(1345, 851);
+            this.calendarUserControl1.Size = new System.Drawing.Size(1292, 851);
             this.calendarUserControl1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(701, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // OutlookCalendarForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1345, 851);
+            this.ClientSize = new System.Drawing.Size(1292, 851);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.calendarUserControl1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "OutlookCalendarForm";
@@ -65,5 +76,6 @@
 
         private CalendarUserControl calendarUserControl1;
         private Telerik.WinControls.Themes.TelerikMetroBlueTheme telerikMetroBlueTheme1;
+        private System.Windows.Forms.Button button1;
     }
 }
